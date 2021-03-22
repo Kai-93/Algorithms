0372c73d4ae0ae2b60e288729a086448fc2698c0
ZhaoKai-Kaiser <kaiseer@yeah.net>
2021-03-22 10:07:45 +0800

    bubble

--- Sort/sort_bubble.js
+++ Sort/sort_bubble.js
@@ -2,7 +2,7 @@
  * @Author: Kaiser
  * @Date: 2021-03-02 13:02:08
  * @Last Modified by: Kaiser
- * @Last Modified time: 2021-03-03 07:39:35
+ * @Last Modified time: 2021-03-22 10:02:06
  * @Description: 冒泡
  * 每一个排序确定一个最值
  */
@@ -21,5 +21,16 @@ function bubble(arr) {
   }
 }
 
-module.exports = bubble;
+// 冒泡 - 升级版
+function bubble(arr) {
+  const length = arr.length;
+  for (let i = length - 1; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        swap(arr, i, j);
+      }
+    }
+  }
+}
 
+module.exports = bubble;
